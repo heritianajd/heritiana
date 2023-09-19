a67255a8939e0606c8c242146e107742c8e6804b
herintsaina <herintsainarak024@gmail.com>
2023-09-19 15:54:51 +0300

    modification des élements

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ let body = document.querySelector('body');
 toggle.addEventListener('click', function(){
     body.classList.toggle('open');
 })
+
+//Copier un texte
+
+
+function copyText(elementId) {
+    const textElement = document.getElementById(elementId);
+    const textToCopy = textElement.textContent;
+
+    const tempElement = document.createElement('textarea');
+    tempElement.value = textToCopy;
+    document.body.appendChild(tempElement);
+    tempElement.select();
+
+    document.execCommand('copy');
+
+    document.body.removeChild(tempElement);
+  }
+
